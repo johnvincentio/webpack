f6f72cb7de8c152e5f4b5f7b3fbd915a8b30717d
johnvincentio <john@johnvincent.io>
2017-10-01 10:05:18 -0400

    web pack

---
+++ play-3/webpack.config.js
@@ -0,0 +1,54 @@
+
+const webpack = require('webpack');
+const path = require('path');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const APP_FOLDER = path.resolve(__dirname, './src');
+const DIST_FOLDER = path.resolve(APP_FOLDER, './dist');
+const DIST_FOLDER_STYLE = path.resolve(DIST_FOLDER, './css');
+
+const config = {
+  entry: ['./src/index.js', './src/scss/index.scss'],
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js',
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    inline: true,
+    port: 8084,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      { // regular css files
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader/url!file-loader',
+          use: ['css-loader'],
+          publicPath: DIST_FOLDER_STYLE,
+        }),
+      },
+      { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin({ // define where to save the file
+      filename: '[name].bundle.css',
+      allChunks: true,
+    }),
+  ],
+};
+
+module.exports = config;
